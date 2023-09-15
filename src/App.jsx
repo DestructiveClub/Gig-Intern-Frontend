0f3672d5cc298b387ab8128b2e1a269a630e8034
ProvLeon <providence.leonard@gmail.com>
2023-09-15 19:04:33 +0100

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
-
+import SignUp from "views/sign-up/Sign-up";
 import AdminLayout from "layouts/admin";
+
 const App = () => {
   return (
     <Routes>
       <Route path="admin/*" element={<AdminLayout />} />
       <Route path="/" element={<Navigate to="/admin" replace />} />
+      <Route path="sign-up" element={<SignUp />} />
     </Routes>
   );
 };
